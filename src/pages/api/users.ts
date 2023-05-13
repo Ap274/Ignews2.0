610125e2f0ceeeb6efc32c4140f59768916180ef
Ap274 <aldopiresaps@gmail.com>
2023-05-13 16:46:30 -0300

    feat: conceptual understanding of authentication and api routes in next.js

---
+++ src/pages/api/users.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    const users = [
+        {
+            id: 1,
+            name: 'Diego',
+        },
+        {
+            id: 2,
+            name: 'Dani',
+        },
+        {
+            id: 3,
+            name: 'Rafa',
+        },
+    ]
+
+    return response.json(users)
+    //localhost:3000/api/users para acesso aos dados
+
+}
