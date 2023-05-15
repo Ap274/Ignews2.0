b8690b12ca5a098527d5c53ec856ac27a7b3fb75
Ap274 <aldopiresaps@gmail.com>
2023-05-15 09:47:34 -0300

    feat: adding user to database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   providers: [
@@ -14,5 +18,30 @@ export const authOptions = {
       }
     }),
   ],
+
+  jwt: {
+    signinKey: process.env.SIGNIN_KEY
+  },
+
+  callbacks: {
+    async signIn({user, account, profile}) {
+      // console.log(user)
+      const { email } = user
+
+      try {
+        // fazendo inserção no banco com o método Create
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }      
+    }
+  }
 }
 export default NextAuth(authOptions)
