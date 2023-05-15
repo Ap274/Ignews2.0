159a30ba16bee913f36ac8be6d1295bdcd2a34a4
Ap274 <aldopiresaps@gmail.com>
2023-05-15 11:15:30 -0300

    feat: generating checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,30 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss'
+
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton( { priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession()
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        // após criação da checkout session, fazer uma requisição para a route subscribe, para criarmos a sessão e redirecionarmos os dados
+        
+        
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
