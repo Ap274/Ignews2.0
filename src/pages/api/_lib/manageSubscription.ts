60404d375df2639a2d23dd3a75ec622c1ae1eb4c
Ap274 <aldopiresaps@gmail.com>
2023-05-16 18:00:41 -0300

    feat: saving events on FaunaDB

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,41 @@
+import { query as q } from 'faunadb'
+
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+) {
+    // console.log(subscriptionId, customerId)
+    // Primeiramente, iremos buscar os dados do usuário
+    const userRef = await fauna.query(
+        q.Select(
+            "ref",
+            q.Get(
+                q.Match(
+                    q.Index('user_by_stripe_customer_id'),
+                    customerId
+                )
+            )
+        )
+    )
+
+    // Agora, precisamos de todos os dados da subscription || subscriptionData só para filtrar os dados mais importates
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id,
+    }
+
+    // Agora, salvar esses dados no FaunaDB
+    await fauna.query(
+        q.Create(
+            q.Collection('subscriptions'),
+            { data: subscriptionData }
+        )
+    )
+}
