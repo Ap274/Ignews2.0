f2e334b3616413cbf49b97a96e582ccdf25208f0
Ap274 <aldopiresaps@gmail.com>
2023-05-22 10:14:20 -0300

    feat: generating static previews

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { getSession, useSession } from "next-auth/react"
 import { RichText } from "prismic-dom"
 import Head from "next/head"
@@ -56,7 +56,7 @@ export default function PostPreview( {post} : PostPreviewProps) {
     )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     return { 
         paths: [],
         fallback: 'blocking'
@@ -86,7 +86,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post
-        }
+        },
+        revalidate: 60 * 30 // 30 minutes
     }
     
 }
